42a74375e074000a62733f3ae71f6dafeba70e52
Julia <julialynn396@gmail.com>
2021-05-07 15:50:45 -0500

    Creating class for todo items

--- app.py
+++ app.py
@@ -6,8 +6,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
 @app.route('/')
 def index():
+    todo_list = Todo.query.all()
     return render_template('base.html')
 
 if __name__ == "__main__":
